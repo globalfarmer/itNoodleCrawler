f88d9318f435aecf07e81a8062a432449e5d424b
globalfarmer <dovietanh.95@gmail.com>
2018-05-06 08:27:48 +0700

    issue #1: create crawl framework

--- routes/slot.js
+++ routes/slot.js
@@ -4,6 +4,7 @@ var mongoose = require('mongoose');
 var cheerio = require('cheerio');
 var querystring = require('querystring');
 var crawler_util = require('../util/crawler_util.js');
+var crypto = require('crypto');
 var options = 
 		{
 			"host": "112.137.129.87",
@@ -11,23 +12,29 @@ var options =
 			"path": "/congdaotao/module/qldt/",
 			"method": "GET"
 		};
-const MAX_SIZE = 10;
-
+const MAX_SIZE = 50000;
 router.get('/', (req, res) => {
 	console.log("call slot crawler");
 	crawler_util
 	.crawl(options)
 	.then((data) => {
 		// console.log(data);
-		var $ = cheerio.load(data);
-		var terms = [];
+		let $ = cheerio.load(data[0]);
+		let terms = [];
 		$("#SinhvienLmh_term_id option").each((idx, el) => {
 			terms.push([$(el).attr('value'), $(el).text()]);
 		});
 		terms = terms.slice(1);
 		console.log(terms);
 		terms.forEach((el, idx) => {
-			params =  
+			let options = 
+			{
+				"host": "112.137.129.87",
+				"port": "443",
+				"path": "/congdaotao/module/qldt/",
+				"method": "GET"
+			};
+			let params =  
 			{
 				'SinhvienLmh[term_id]': el[0],
                 'SinhvienLmh_page': 1,
@@ -37,7 +44,9 @@ router.get('/', (req, res) => {
 			crawler_util
 			.crawl(options)
 			.then((data) => {
-				console.log(data)
+				// console.log(data);
+				console.log(data[1]);
+				// console.log(crypto.createHash('md5').update(data[0]).digest('hex'));
 			})
 			.catch((e) => console.log(e));
 		});
--- util/crawler_util.js
+++ util/crawler_util.js
@@ -1,22 +1,25 @@
 var querystring = require('querystring'),
 	http = require('http'),
-	https = require('https');
+	https = require('https'),
+	crypto = require('crypto');
 module.exports = {
 	crawl: function(options, params) {
-        var timeLabel = 'crawl_' + (new Date()).getTime();
+        let timeLabel = 'crawl_' + (new Date()).getTime();
         console.log(options);
         console.time(timeLabel);
+        let md5sum = crypto.createHash('md5');
         return new Promise((resolve, reject) => {
-            var htmlContent = "";
-            var pro = options.port == 443 ? https : http;
-            var req = pro.request(options, (res) => {
+            let htmlContent = "";
+            let pro = options.port == 443 ? https : http;
+            let req = pro.request(options, (res) => {
                 res.setEncoding('utf8');
                 res.on('data', (chunk) => {
                     htmlContent += chunk;
+                    md5sum.update(chunk);
                 });
                 res.on('end', () => {
                     console.timeEnd(timeLabel);
-                    resolve(htmlContent);
+                    resolve([htmlContent, md5sum.digest('hex')]);
                 });
             });
             if(options.method.toLowerCase() === 'post' )
